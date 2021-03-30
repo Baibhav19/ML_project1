aa22ca649fe48172b29fe49b0cdecf5eaf49bbff
LPT-2517 <LPT-2517@LPT2517.newgen.co.in>
2021-03-31 00:38:30 +0530

    train and evaluate

--- src/load_and_split.py
+++ src/load_and_split.py
@@ -1,28 +1,27 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-#read params
-# process data
-
-
-# In[51]:
-
-
 import os
 import yaml
 import pandas as pd
 import numpy as np
 import argparse
+from sklearn.model_selection import train_test_split
 
-class process_data:
+class load_and_split_data:
     
     def get_data_func(self, data_path):
         df = pd.read_csv(data_path, sep=',')
         return df
-    
+
+    def split_train_test_func(self, config_path):
+        train_path = config_path['split_data']["train_path"]
+        test_path = config_path['split_data']["test_path"]
+        random_state = config_path['base']["random_state"]
+        load_path = config_path['load_data']["raw_dataset_csv"]
+        df = pd.read_csv(load_path, sep=",")
+        test_size = config_path['split_data']["test_size"]
+        train, test = train_test_split(df, random_state = random_state, test_size=test_size)
+        train.to_csv(train_path, index=False, sep=',')
+        test.to_csv(test_path, index=False, sep=',')
+
     def split_raw_data(self, df, config_path):
         cols = [col.replace(' ', '_') for col in df.columns]
         print(cols)
@@ -37,24 +36,11 @@ class process_data:
         print(self.raw_data)
         df = self.get_data_func(self.raw_data['data_source']['s3_source'])
         self.split_raw_data(df, self.raw_data['load_data']['raw_dataset_csv'])
+        self.split_train_test_func(self.raw_data)
 
 if __name__ == "__main__":
-	process_data()
+	load_and_split_data()
 		
-# In[52]:
-
-
-#process_data()
-
-
-# In[44]:
-
-
-#"a a".replace(" ", "_")
-
-
-# In[ ]:
-
 
 
 
